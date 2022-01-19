98687f13a6caf30653d06f06f81cf867cf2abe3b
HMD777 <a.davurov@gmail.com>
2022-01-19 23:50:05 +0600

    Homework 2

---
+++ Davurov_Abdukhamid_dz_1_1/Tasl_1_2.py
@@ -0,0 +1,40 @@
+""""
+number_min = 1
+number_max = 1000
+
+def sum_list_1(dataset: list) -> int:
+    number_find = number_min
+    number_list=[]
+    list_indx=0
+    new_list=[]
+    while number_find<number_max:
+        number_find += 1
+        number_find **= 3
+        number_list.append = (number_find)
+    while list_indx < len(number_list)
+
+"""
+asd = [10,20,30]
+print(asd)
+
+
+""""
+
+    """Вычисляет сумму чисел списка dataset, сумма цифр которых делится нацело на 7"""
+    # место для написания кода
+    return 1  # Верните значение полученной суммы
+
+
+def sum_list_2(dataset: list) -> int:
+    """К каждому элементу списка добавляет 17 и вычисляет сумму чисел списка,
+        сумма цифр которых делится нацело на 7"""
+    # место для написания кода
+    return 1  # Верните значение полученной суммы
+
+
+my_list = []  # Соберите нужный список по заданию
+result_1 = sum_list_1(my_list)
+print(result_1)
+result_2 = sum_list_2(my_list)
+print(result_2)
+"""
