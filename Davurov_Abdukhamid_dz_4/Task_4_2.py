45efe6d50b9addb0eb107cad7ad0d0e150a5da5c
Abdukhamid <a.davurov@gmail.com>
2022-02-02 22:49:02 +0600

    Homework 4 done

---
+++ Davurov_Abdukhamid_dz_4/Task_4_2.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def currency_rates(code: str) -> float:
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    fx_data = {}
+    list_cur = response.text.split('ID=')
+    del list_cur[0]
+    for i in list_cur:
+        currency_v = i[(i.find('<Value>')+7):i.find('</Value>')]
+        currency_v = float(currency_v.replace(',', '.'))
+        nominal_v = int(i[(i.find('<Nominal>')+9):i.find('</Nominal>')])
+        fx_data[(i[(i.find('<CharCode>')+10):i.find('</CharCode>')])] = currency_v/nominal_v
+    result_value = fx_data.get(code.upper())
+    return result_value
+
+
+print(currency_rates("AUD"))
+print(currency_rates("noname"))
+print(currency_rates("USD"))
